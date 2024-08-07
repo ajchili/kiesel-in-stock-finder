0e0784be7fdcad6a3534ebf93ede54a55edc9ee3
Kirin Patel <kirinpatel@gmail.com>
2024-08-07 19:14:35 -0400

    🔢 add filter normalization

--- backend/kiesel.py
+++ backend/kiesel.py
@@ -2,6 +2,97 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
+spec_category_mapping = {
+    'price': 'general',
+    'model': 'general',
+    'modelName': 'general',
+    'bridge': 'hardware',
+    'serialNumber': None,
+    'stringCount': 'general',
+    'dexterity': 'general',
+    'multiscale': 'general',
+    'case': 'general',
+    'construction': 'general',
+    'scaleLength': 'general',
+    'bevel': 'body',
+    'body': 'body',
+    'bodyConstruction': 'body',
+    'topWood': 'body',
+    'woodTreatment': 'body',
+    'threePieceBody': 'body',
+    'finish': 'body',
+    'customShopFinish': 'body',
+    'crackle': 'body',
+    'cali': 'body',
+    'burstEdges': 'body',
+    'topCoat': 'body',
+    'pickguard': 'body',
+    'rearFinish': 'body',
+    'binding': 'body',
+    'headstock': 'neck',
+    'headstockOverlay': 'neck',
+    'headstockFinish': 'neck',
+    'trussRodCover': 'neck',
+    'neck': 'neck',
+    'neckFinish': 'neck',
+    'neckProfile': 'neck',
+    'numFrets': 'neck',
+    'fingerboard': 'neck',
+    'fingerboardRadius': 'neck',
+    'frets': 'neck',
+    'inlays': 'neck',
+    'inlayMaterial': 'neck',
+    'hardware': 'hardware',
+    'logo': 'neck',
+    'strapButtons': 'hardware',
+    'tuners': 'neck',
+    'knobs': 'hardware',
+    'strings': 'hardware',
+    'pickupConfiguration': 'electronics',
+    'bridgePickup': 'electronics',
+    'neckPickup': 'electronics',
+    'middlePickup': 'electronics',
+    'pickupColor': 'electronics',
+    'polePieces': 'electronics',
+    'midi': None,
+    'controls': 'electronics',
+    'notes': None,
+    'optionFifty': None,
+    'nonReturnable': None,
+    'extra': None,
+    'optionFiftyOne': None,
+    'weight': None,
+}
+
+
+def normalize_spec_filter_values(specName, variants):
+    if specName in ['price']:
+        return {'type': 'number', 'variants': list(map(lambda num: float(num), variants))}
+
+    return {'type': 'string', 'variants': variants}
+
+
+def normalize_spec_filters(specs):
+    normalized_filters = {
+        'other': {}
+    }
+
+    for spec in specs:
+        category = spec_category_mapping.get(spec, None)
+
+        if category is None:
+            normalized_filters['other'][spec] = normalize_spec_filter_values(
+                specName=spec, variants=specs[spec])
+            continue
+
+        if category not in normalized_filters:
+            normalized_filters[category] = {}
+
+        normalized_filters[category][spec] = normalize_spec_filter_values(
+            specName=spec, variants=specs[spec])
+
+    return normalized_filters
+
 
 def get_in_stock_instruments():
     request = requests.get("https://www.kieselguitars.com/in-stocks")
@@ -32,6 +123,4 @@ def get_in_stock_instruments():
         specs_on_in_stock_instruments[spec] = list(
             specs_on_in_stock_instruments[spec])
 
-    specs_on_in_stock_instruments
-
-    return in_stock_instruments, specs_on_in_stock_instruments
+    return in_stock_instruments, normalize_spec_filters(specs_on_in_stock_instruments)
