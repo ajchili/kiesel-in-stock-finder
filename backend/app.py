a3d1d63e3d9f5d037c6a2371b37f731528f126ea
Kirin Patel <kirinpatel@gmail.com>
2024-08-06 22:41:14 -0400

    🔧 "fix" CORS

--- backend/app.py
+++ backend/app.py
@@ -16,4 +16,6 @@ def get_guitars():
     resp = make_response(json.dumps(
         {"instruments": instruments, "specs": specs}), 200)
     resp.headers['Cache-Control'] = 'max-age=300'
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     return resp
