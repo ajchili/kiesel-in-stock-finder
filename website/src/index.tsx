15d83a267058c400808fd375dd20e879555262f5
Kirin Patel <kirinpatel@gmail.com>
2024-08-09 23:39:40 -0400

    🔧 add tailwind

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { Everything } from "./components/Everything";
+import { Everything } from "./components/Everything.js";
+
+import "./index.css";
 
 let $app = document.getElementById("app");
 
